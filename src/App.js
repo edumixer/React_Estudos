7a659c5763caf22e81cde54a27e390b1a739e1d7
edumixer <edu.cena.wf@gmail.com>
2021-07-21 10:39:46 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import { Component } from 'react'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      name: 'Eduardo Franco'
-    };
+class App extends React.Component {
+  state = {
+    counter: 0,
+    posts: [{
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      }, {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      }, {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      }
+    ],
+  };
+
+  componentDidMount() {
   }
 
+  componentDidUpdate() {
+  }
+
+  componentWillUnmount() {
+  }
+  
+
   render() {
-    const { name } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {name}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        )
+        )}
       </div>
     );
   }
