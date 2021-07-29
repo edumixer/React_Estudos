6971c48bd3b88c6d8d8fb5d4989b89a3597c400a
edumixer <edu.cena.wf@gmail.com>
2021-07-29 11:16:41 -0300

    useRef - React Hook

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,12 +1,12 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({post}) => {
+const Post = ({post, handleClick}) => {
   console.log('Filho renderizou!');
   return (
     <div key={post.id} className="post">
-    <h1>{post.title}</h1>
+    <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>{post.title}</h1>
     <p>{post.body}</p>
   </div>
   );
@@ -17,34 +17,51 @@ Post.propTypes = {
     id: P.number,
     title: P.string,
     body: P.string,
-  })
+  }),
+  onClick: P.func,
 }
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
-  //console.log('Pai renderizou!');
+  const input = useRef(null);
+  const counter = useRef(0);
+
+  console.log('Pai renderizou!');
 
   // Component did mount
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(r => r.json())
-      .then(r => setPosts(r));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(r => r.json())
+    .then(r => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    counter.current++;
+  }); 
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h1>Renderizou: {counter.current}x</h1>
       <p>
-        <input 
+        <input
+          ref={input}
           type="search" 
           value={value} 
           onChange={(e) => setValue(e.target.value)} />
       </p>
       {useMemo(() => {
         return posts.length > 0 && posts.map((post) => {
-          return <Post key ={post.id} post={post} />
+          return <Post key={post.id} post={post} handleClick={handleClick}/>
         })
       }, [posts])}
       {posts.length <= 0 && <p>Ainda não existem posts.</p>}
