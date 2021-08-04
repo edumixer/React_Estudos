5c9d2b906226ebf265e65f15bc7b6588f4528c9e
edumixer <edu.cena.wf@gmail.com>
2021-08-04 15:48:40 -0300

    Context API + Hooks (unindo as ideias) - Parte 2

--- project_1/src/templates/Home/index.jsx
+++ project_1/src/templates/Home/index.jsx
@@ -1,23 +1,11 @@
-import React, { Suspense, useState } from 'react';
-// import LazyComponent from './lazy-component';
-
-const loadComponent = () => {
-  console.log('Componente carregando...');
-  return import('./lazy-component');
-};
-const LazyComponent = React.lazy(loadComponent);
+import { useCounterContext } from '../../contexts/CounterContexts';
 
 export const Home = () => {
-  const [show, setShow] = useState(false);
+  const [state, dispatch] = useCounterContext();
 
   return (
     <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
-          Show {show ? 'LC on screen' : 'LC is off screen'}
-        </button>
-      </p>
-      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+      <h1>Oi</h1>
     </div>
   );
 };
