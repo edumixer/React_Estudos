4ca937f9d2c952badf8b1578fd6a1084e1390334
edumixer <edu.cena.wf@gmail.com>
2021-08-04 10:00:43 -0300

    useLayoutEffect

--- project_1/src/templates/Home/index.jsx
+++ project_1/src/templates/Home/index.jsx
@@ -1,160 +1,50 @@
-import { useEffect, useState, useCallback } from 'react'
-
-import './styles.css';
-
-import { Posts } from '../../components/Posts';
-import { loadPosts } from '../../utils/load-posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
+import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
 
 export const Home = () => {
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const divRef = useRef();
 
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(2);
-  const [searchValue, setSearchValue] = useState('');
+  useLayoutEffect(() => {
+    const now = Date.now();
+    while (Date.now() < now + 300);
+    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
+  });
 
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const filteredPosts = !!searchValue ? 
-  allPosts.filter(post => {
-    return post.title.toLowerCase().includes(
-      searchValue.toLowerCase()
-    );
-  })
-  : posts;
-
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
-
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, [])
-
-  useEffect(() => {
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
-
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    
-    posts.push(...nextPosts);
-
-    setPosts(posts);
-    setPage(nextPage);
-  }
-
-  const handleChange = (e) => {
-    const value  = e.target;
-    setSearchValue(value);
-  }
+  const handleClick = () => {
+    setCounted((c) => [...c, +c.slice(-1) + 1]);
+    divRef.current.handleClick();
+  };
 
   return (
-    <section className="container">
-      <div className="search-container">          
-        {!!searchValue && (
-          <h1>Search value: {searchValue}</h1>
-        )}
-        
-        <TextInput searchValue={searchValue} handleChange={handleChange}/>
-      </div>
-
-      {filteredPosts.length > 0 && (
-        <Posts posts={filteredPosts}/>
-      )}
-
-      {filteredPosts.length === 0 && (
-        <p>Não existem posts =(</p>
-      )}
-      
-      <div className="button-container">
-        {!searchValue && (
-          <Button
-            text="Load more posts"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
-      </div>
-    </section>
+    <>
+      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
+      <DisplayCounted counted={counted} ref={divRef} />
+    </>
   );
+};
 
-}
-// export class Home2 extends Component {
-//   state = {
-//     posts: [],
-//     allPosts: [],
-//     page: 0,
-//     postsPerPage: 10,
-//     searchValue: ''
-//   };
+export const DisplayCounted = forwardRef(function DisplayCounted({ counted }, ref) {
+  const [rand, setRand] = useState('0.24');
+  const divRef = useRef();
 
-//   async componentDidMount() {
-//     await this.loadPosts();
-//   }
+  const handleClick = () => {
+    setRand(Math.random().toFixed(2));
+  };
 
-//   loadPosts = async () => {
-//     const { page, postsPerPage } = this.state;
-//     const postsAndPhotos = await loadPosts();
-//     this.setState({
-//       posts: postsAndPhotos.slice(page, postsPerPage),
-//       allPosts: postsAndPhotos
-//     });
-//   } 
+  useImperativeHandle(ref, () => ({
+    handleClick,
+    divRef: divRef.current,
+  }));
 
-//   loadMorePosts = () => {
-//     const { page, postsPerPage, allPosts, posts } = this.state;
-//     const nextPage = page + postsPerPage;
-//     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    
-//     posts.push(...nextPosts);
-
-//     this.setState({ posts, page: nextPage })
-//   }
-
-//   handleChange = (e) => {
-//     const value  = e.target;
-//     this.setState({ searchValue: value });
-//   }
-
-//   render() {
-//     const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-//     const noMorePosts = page + postsPerPage >= allPosts.length;
-
-//     const filteredPosts = !!searchValue ? 
-//     allPosts.filter(post => {
-//       return post.title.toLowerCase().includes(
-//         searchValue.toLowerCase()
-//       );
-//     })
-//     : posts;
-
-//     return (
-//       <section className="container">
-//         <div className="search-container">          
-//           {!!searchValue && (
-//             <h1>Search value: {searchValue}</h1>
-//           )}
-          
-//           <TextInput searchValue={searchValue} handleChange={this.handleChange}/>
-//         </div>
-
-//         {filteredPosts.length > 0 && (
-//           <Posts posts={filteredPosts}/>
-//         )}
-        
-//         <div className="button-container">
-//           {!searchValue && (
-//             <Button
-//               text="Load more posts"
-//               onClick={this.loadMorePosts}
-//               disabled={noMorePosts}
-//             />
-//           )}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
+  return (
+    <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+      {counted.map((c) => {
+        return (
+          <p onClick={handleClick} key={`c-${c}`}>
+            {c} +++ {rand}
+          </p>
+        );
+      })}
+    </div>
+  );
+});
