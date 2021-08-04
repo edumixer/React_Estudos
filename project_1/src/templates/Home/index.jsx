8bf1c927fe292cd7b3388b4319fba8f0d9bd71ee
edumixer <edu.cena.wf@gmail.com>
2021-08-04 14:32:47 -0300

    Padrão Compound Components e Context API

--- project_1/src/templates/Home/index.jsx
+++ project_1/src/templates/Home/index.jsx
@@ -1,40 +1,56 @@
-import { useDebugValue, useEffect, useState } from 'react';
+// Compound Components
+import { Children, cloneElement, createContext, useContext, useState } from 'react';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
-
-  useDebugValue(`Query: ${queryValue}`, (name) => {
-    return name + ' modificado';
-  });
-
-  useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
-
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
+const s = {
+  style: {
+    fontSize: '60px',
+  },
 };
 
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
+};
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
+const P = ({ children }) => <p {...s}>{children}</p>;
+
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
-  const small = useMediaQuery('(max-width: 321px)');
-
-  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'purple' : null;
-
-  return <div style={{ fontSize: '60px', background }}>Oi</div>;
+  return (
+    <TurnOnOff>
+      <div>
+        <header>
+          {' '}
+          <TurnedOff>
+            <P>Aqui vem as coisas do OFF.</P>
+          </TurnedOff>
+        </header>
+        <section>
+          <TurnedOn>
+            <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+          </TurnedOn>
+        </section>
+      </div>
+      <TurnButton {...s} />
+    </TurnOnOff>
+  );
 };
